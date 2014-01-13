88dbe07e31b9e9efa01265182cfd93d42b143dcd
snyh <snyh@snyh.org>
2014-01-13 22:10:26 +0800

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+What it will be?
+===============
+* logout/shutdown/reboot dialog
+* autostart manager
+* session manager
+* xsettings-daemon
+* DDE core component scheduler
+
+
+the urgent tasks
+================
+* autostart support
+* dialog support
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import "os/exec"
+import "time"
+
+func main() {
+	go exec.Command("/usr/bin/compiz").Run()
+	<-time.After(time.Millisecond * 200)
+
+	go exec.Command("/usr/bin/gnome-settings-daemon").Run()
+	<-time.After(time.Millisecond * 100)
+
+	go exec.Command("/usr/lib/deepin-daemon/binding-manager").Run()
+	go exec.Command("/usr/lib/deepin-daemon/display").Run()
+	<-time.After(time.Millisecond * 20)
+
+	go exec.Command("/usr/bin/dock").Run()
+	<-time.After(time.Millisecond * 200)
+	go exec.Command("/usr/bin/dapptray").Run()
+	<-time.After(time.Millisecond * 20)
+
+	go exec.Command("/usr/bin/desktop").Run()
+	<-time.After(time.Millisecond * 3000)
+
+	go exec.Command("/usr/bin/dss").Run()
+	go exec.Command("/usr/bin/launcher", "-H").Run()
+
+	<-time.After(time.Millisecond * 3000)
+	go exec.Command("/usr/bin/skype").Run()
+
+	for {
+		<-time.After(time.Millisecond * 1000)
+	}
+}
