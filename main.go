dcfa59c635c27a610d3900e458e6ac7d7a683e89
snyh <snyh@snyh.org>
2014-03-03 11:00:31 +0800

    fix startup schedule

--- main.go
+++ main.go
@@ -1,71 +1,72 @@
 package main
 
 import (
-        "flag"
-        "fmt"
-        "os/exec"
-        "time"
+	"flag"
+	"fmt"
+	"os/exec"
+	"time"
 
-        "dlib"
+	"dlib"
 )
 
 func testStartManager() {
-        startStartManager()
-        // args := make([]*gio.File, 0)
-        // for _, name := range m.AutostartList() {
-        // 	fmt.Println("launch", name)
-        // 	m.Launch(name, args)
-        // }
-        dlib.StartLoop()
+	startStartManager()
+	// args := make([]*gio.File, 0)
+	// for _, name := range m.AutostartList() {
+	// 	fmt.Println("launch", name)
+	// 	m.Launch(name, args)
+	// }
+	dlib.StartLoop()
 }
 
 func test() {
-        testStartManager()
+	testStartManager()
 }
 
 var (
-        debug           bool    = false
-        notStartInitPro bool    = false
+	debug           bool = false
+	notStartInitPro bool = false
 )
 
 func main() {
-        // test()
-        // return
+	// test()
+	// return
 
-        flag.BoolVar(&debug, "d", false, "debug")
-        flag.BoolVar(&notStartInitPro, "n", false, "not start")
+	flag.BoolVar(&debug, "d", false, "debug")
+	flag.BoolVar(&notStartInitPro, "n", false, "not start")
 
-        flag.Parse()
-        fmt.Println("debug:", debug)
-        fmt.Println("notStartInitPro:", notStartInitPro)
+	flag.Parse()
+	fmt.Println("debug:", debug)
+	fmt.Println("notStartInitPro:", notStartInitPro)
 
-        startXSettings()
-        startStartManager()
+	startXSettings()
 
-        if !notStartInitPro {
-                go exec.Command("/usr/bin/compiz").Run()
-                <-time.After(time.Millisecond * 200)
+	// Session Manager
+	startSession()
 
-                go exec.Command("/usr/lib/deepin-daemon/keybinding").Run()
-                go exec.Command("/usr/lib/deepin-daemon/display").Run()
-                <-time.After(time.Millisecond * 20)
+	if !notStartInitPro {
+		go exec.Command("/usr/bin/compiz").Run()
+		<-time.After(time.Millisecond * 200)
 
-                go exec.Command("/usr/bin/dock").Run()
-                <-time.After(time.Millisecond * 200)
-                go exec.Command("/usr/bin/dapptray").Run()
-                <-time.After(time.Millisecond * 20)
+		go exec.Command("/usr/lib/deepin-daemon/keybinding").Run()
+		go exec.Command("/usr/lib/deepin-daemon/display").Run()
+		<-time.After(time.Millisecond * 20)
 
-                go exec.Command("/usr/bin/desktop").Run()
-                <-time.After(time.Millisecond * 3000)
+		go exec.Command("/usr/bin/dock").Run()
+		<-time.After(time.Millisecond * 200)
+		go exec.Command("/usr/bin/dapptray").Run()
+		<-time.After(time.Millisecond * 20)
 
-                go exec.Command("/usr/lib/deepin-daemon/launcher-daemon").Run()
-                <-time.After(time.Millisecond * 3000)
+		go exec.Command("/usr/bin/desktop").Run()
+		<-time.After(time.Millisecond * 3000)
 
-                // Session Manager
-                startSession()
-        }
+		go exec.Command("/usr/lib/deepin-daemon/launcher-daemon").Run()
+		<-time.After(time.Millisecond * 3000)
+	}
 
-        for {
-                <-time.After(time.Millisecond * 1000)
-        }
+	startStartManager()
+
+	for {
+		<-time.After(time.Millisecond * 1000)
+	}
 }
