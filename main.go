e02dd2f649a1bb77d56bcbc50df2673ebee13259
bluth653426595 <ycl_pj@163.com>
2014-04-04 13:22:43 +0800

    main.go:add zone-daemon

--- main.go
+++ main.go
@@ -75,6 +75,9 @@ func main() {
 		go exec.Command("/usr/lib/deepin-daemon/launcher-daemon").Run()
 		<-time.After(time.Millisecond * 3000)
 
+		go exec.Command("/usr/lib/deepin-daemon/zone-daemon").Run()
+		<-time.After(time.Millisecond * 3000)
+
 	}
 
 	startStartManager()
