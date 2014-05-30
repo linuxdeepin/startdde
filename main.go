211c65512954539d7eabeafd70da2b2155996eb6
kosl90 <kos1990l@gmail.com>
2014-05-30 14:04:32 +0800

    rename launcher-daemon to launcher

--- main.go
+++ main.go
@@ -82,7 +82,7 @@ func main() {
 		go exec.Command("/usr/bin/dde-desktop").Run()
 		<-time.After(time.Millisecond * 3000)
 
-		go exec.Command("/usr/lib/deepin-daemon/launcher-daemon").Run()
+		go exec.Command("/usr/lib/deepin-daemon/launcher").Run()
 		<-time.After(time.Millisecond * 3000)
 
 		go exec.Command("/usr/lib/deepin-daemon/zone-settings").Run()
