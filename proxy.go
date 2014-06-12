e9c3a9cf7b4d1b1822558f3d74a2440b32030ca1
Xu FaSheng <fasheng.xu@gmail.com>
2014-06-12 15:58:13 +0800

    proxy: support proxy method and automatic proxy

--- proxy.go
+++ proxy.go
@@ -27,16 +27,25 @@ import (
 )
 
 const (
+	envAutoProxy  = "auto_proxy"
 	envHttpProxy  = "http_proxy"
 	envHttpsProxy = "https_proxy"
 	envFtpProxy   = "ftp_proxy"
 	envSocksProxy = "socks_proxy"
 
 	gsettingsIdProxy = "com.deepin.dde.proxy"
-	gkeyHttpProxy    = "http-proxy"
-	gkeyHttpsProxy   = "https-proxy"
-	gkeyFtpProxy     = "ftp-proxy"
-	gkeySocksProxy   = "socks-proxy"
+	gkeyProxyMethod  = "proxy-method"
+
+	proxyMethodNone   = "none"
+	proxyMethodManual = "manual"
+	proxyMethodAuto   = "auto"
+
+	gkeyAutoProxy = "auto-proxy"
+
+	gkeyHttpProxy  = "http-proxy"
+	gkeyHttpsProxy = "https-proxy"
+	gkeyFtpProxy   = "ftp-proxy"
+	gkeySocksProxy = "socks-proxy"
 )
 
 var (
@@ -49,21 +58,39 @@ func startProxy() {
 }
 
 func updateProxyEnvs() {
-	httpProxy := proxySettings.GetString(gkeyHttpProxy)
-	os.Setenv(envHttpProxy, httpProxy)
-	Logger.Debug("update proxy envs, httpProxy:", httpProxy)
+	proxyMethod := proxySettings.GetString(gkeyProxyMethod)
+	switch proxyMethod {
+	case proxyMethodNone:
+		os.Setenv(envAutoProxy, "")
+		os.Setenv(envHttpProxy, "")
+		os.Setenv(envHttpsProxy, "")
+		os.Setenv(envFtpProxy, "")
+		os.Setenv(envSocksProxy, "")
+	case proxyMethodAuto:
+		autoProxy := proxySettings.GetString(gkeyAutoProxy)
+		os.Setenv(envAutoProxy, autoProxy)
+		os.Setenv(envHttpProxy, "")
+		os.Setenv(envHttpsProxy, "")
+		os.Setenv(envFtpProxy, "")
+		os.Setenv(envSocksProxy, "")
+	case proxyMethodManual:
+		os.Setenv(envAutoProxy, "")
+		httpProxy := proxySettings.GetString(gkeyHttpProxy)
+		os.Setenv(envHttpProxy, httpProxy)
+		Logger.Debug("update proxy envs, httpProxy:", httpProxy)
 
-	httpsProxy := proxySettings.GetString(gkeyHttpsProxy)
-	os.Setenv(envHttpsProxy, httpsProxy)
-	Logger.Debug("update proxy envs, httpsProxy:", httpsProxy)
+		httpsProxy := proxySettings.GetString(gkeyHttpsProxy)
+		os.Setenv(envHttpsProxy, httpsProxy)
+		Logger.Debug("update proxy envs, httpsProxy:", httpsProxy)
 
-	ftpProxy := proxySettings.GetString(gkeyFtpProxy)
-	os.Setenv(envFtpProxy, ftpProxy)
-	Logger.Debug("update proxy envs, ftpProxy:", ftpProxy)
+		ftpProxy := proxySettings.GetString(gkeyFtpProxy)
+		os.Setenv(envFtpProxy, ftpProxy)
+		Logger.Debug("update proxy envs, ftpProxy:", ftpProxy)
 
-	socksProxy := proxySettings.GetString(gkeySocksProxy)
-	os.Setenv(envSocksProxy, socksProxy)
-	Logger.Debug("update proxy envs, socksProxy:", socksProxy)
+		socksProxy := proxySettings.GetString(gkeySocksProxy)
+		os.Setenv(envSocksProxy, socksProxy)
+		Logger.Debug("update proxy envs, socksProxy:", socksProxy)
+	}
 }
 
 func listenProxyGsettings() {
