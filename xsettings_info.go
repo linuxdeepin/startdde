8405282d96651a5dba58e951f11a6c73ec14d0ed
jouyouyun <jouyouwen717@gmail.com>
2015-04-21 11:38:12 +0800

    xsettings: refactor
    - rewrite xsetting reader and writer
    - add xsetiings gschema
    
    Change-Id: Id6834a857bec69bb318fba288fdda809e6b0ddca

--- xsettings_info.go
+++ xsettings_info.go
@@ -1,45 +1,302 @@
 package main
 
-var xsKeyMap = map[string]string{
-        "xft-antialias":                 "Xft/Antialias",
-        "gtk-recentfilesmaxage":         "Gtk/RecentFilesMaxAge",
-        "net-enableinputfeedbacksounds": "Net/EnableInputFeedbackSounds",
-        "gtk-fontname":                  "Gtk/FontName",
-        "net-doubleclicktime":           "Net/DoubleClickTime",
-        "gtk-shellshowsappmenu":         "Gtk/ShellShowsAppMenu",
-        "xft-hinting":                   "Xft/Hinting",
-        "gtk-modules":                   "Gtk/Modules",
-        "gtk-imstatusstyle":             "Gtk/IMStatusStyle",
-        "gtk-cursorthemename":           "Gtk/CursorThemeName",
-        "gtk-immodule":                  "Gtk/IMModule",
-        "gtk-canchangeaccels":           "Gtk/CanChangeAccels",
-        "gtk-toolbarstyle":              "Gtk/ToolbarStyle",
-        "xft-rgba":                      "Xft/RGBA",
-        "gtk-colorpalette":              "Gtk/ColorPalette",
-        "gtk-showunicodemenu":           "Gtk/ShowUnicodeMenu",
-        "xft-hintstyle":                 "Xft/HintStyle",
-        "net-enableeventsounds":         "Net/EnableEventSounds",
-        "gtk-menubaraccel":              "Gtk/MenuBarAccel",
-        "gtk-toolbariconsize":           "Gtk/ToolbarIconSize",
-        "gtk-buttonimages":              "Gtk/ButtonImages",
-        "gtk-impreeditstyle":            "Gtk/IMPreeditStyle",
-        "gtk-timeoutinitial":            "Gtk/TimeoutInitial",
-        "gtk-cursorblinktimeout":        "Gtk/CursorBlinkTimeout",
-        "net-dnddragthreshold":          "Net/DndDragThreshold",
-        "gtk-colorscheme":               "Gtk/ColorScheme",
-        "gtk-cursorthemesize":           "Gtk/CursorThemeSize",
-        "xft-dpi":                       "Xft/DPI",
-        "gtk-menuimages":                "Gtk/MenuImages",
-        "gtk-keythemename":              "Gtk/KeyThemeName",
-        "gtk-enableanimations":          "Gtk/EnableAnimations",
-        "gtk-recentfilesenabled":        "Gtk/RecentFilesEnabled",
-        "net-soundthemename":            "Net/SoundThemeName",
-        "net-cursorblinktime":           "Net/CursorBlinkTime",
-        "gtk-showinputmethodmenu":       "Gtk/ShowInputMethodMenu",
-        "net-themename":                 "Net/ThemeName",
-        "gtk-timeoutrepeat":             "Gtk/TimeoutRepeat",
-        "net-iconthemename":             "Net/IconThemeName",
-        "net-fallbackicontheme":         "Net/FallbackIconTheme",
-        "net-cursorblink":               "Net/CursorBlink",
-        "gtk-automnemonics":             "Gtk/AutoMnemonics",
+import (
+	"pkg.linuxdeepin.com/lib/gio-2.0"
+)
+
+const (
+	gsKeyTypeBool int = iota + 1
+	gsKeyTypeInt
+	gsKeyTypeString
+)
+
+type typeGSKeyInfo struct {
+	gsKey  string
+	xsKey  string
+	gsType int
+}
+
+type typeGSKeyInfos []typeGSKeyInfo
+
+var gsInfos = typeGSKeyInfos{
+	{
+		gsKey:  "theme-name",
+		xsKey:  "Net/ThemeName",
+		gsType: gsKeyTypeString,
+	},
+	{
+		gsKey:  "icon-theme-name",
+		xsKey:  "Net/IconThemeName",
+		gsType: gsKeyTypeString,
+	},
+	{
+		gsKey:  "fallback-icon-theme",
+		xsKey:  "Net/FallbackIconTheme",
+		gsType: gsKeyTypeString,
+	},
+	{
+		gsKey:  "sound-theme-name",
+		xsKey:  "Net/SoundThemeName",
+		gsType: gsKeyTypeString,
+	},
+	{
+		gsKey:  "gtk-theme-name",
+		xsKey:  "Gtk/ThemeName",
+		gsType: gsKeyTypeString,
+	},
+	{
+		gsKey:  "gtk-cursor-theme-name",
+		xsKey:  "Gtk/CursorThemeName",
+		gsType: gsKeyTypeString,
+	},
+	{
+		gsKey:  "gtk-font-name",
+		xsKey:  "Gtk/FontName",
+		gsType: gsKeyTypeString,
+	},
+	{
+		gsKey:  "gtk-key-theme-name",
+		xsKey:  "Gtk/KeyThemeName",
+		gsType: gsKeyTypeString,
+	},
+	{
+		gsKey:  "gtk-color-palette",
+		xsKey:  "Gtk/ColorPalette",
+		gsType: gsKeyTypeString,
+	}, //deprecated
+	{
+		gsKey:  "gtk-toolbar-style",
+		xsKey:  "Gtk/ToolbarStyle",
+		gsType: gsKeyTypeString,
+	}, //deprecated
+	{
+		gsKey:  "gtk-toolbar-icon-size",
+		xsKey:  "Gtk/ToolbarIconSize",
+		gsType: gsKeyTypeString,
+	}, //deprecated
+	{
+		gsKey:  "gtk-color-scheme",
+		xsKey:  "Gtk/ColorScheme",
+		gsType: gsKeyTypeString,
+	}, //deprecated
+	{
+		gsKey:  "gtk-im-preedit-style",
+		xsKey:  "Gtk/IMPreeditStyle",
+		gsType: gsKeyTypeString,
+	}, //deprecated
+	{
+		gsKey:  "gtk-im-status-style",
+		xsKey:  "Gtk/IMStatusStyle",
+		gsType: gsKeyTypeString,
+	}, //deprecated
+	{
+		gsKey:  "gtk-im-module",
+		xsKey:  "Gtk/IMModule",
+		gsType: gsKeyTypeString,
+	},
+	{
+		gsKey:  "gtk-modules",
+		xsKey:  "Gtk/Modules",
+		gsType: gsKeyTypeString,
+	},
+	{
+		gsKey:  "gtk-menubar-accel",
+		xsKey:  "Gtk/MenuBarAccel",
+		gsType: gsKeyTypeString,
+	},
+	{
+		gsKey:  "xft-hintstyle",
+		xsKey:  "Xft/HintStyle",
+		gsType: gsKeyTypeString,
+	},
+	{
+		gsKey:  "xft-rgba",
+		xsKey:  "Xft/RGBA",
+		gsType: gsKeyTypeString,
+	},
+	{
+		gsKey:  "cursor-blink-time",
+		xsKey:  "Net/CursorBlinkTime",
+		gsType: gsKeyTypeInt,
+	},
+	{
+		gsKey:  "gtk-cursor-blink-timeout",
+		xsKey:  "Net/CursorBlinkTimeout",
+		gsType: gsKeyTypeInt,
+	},
+	{
+		gsKey:  "double-click-time",
+		xsKey:  "Net/DoubleClickTime",
+		gsType: gsKeyTypeInt,
+	},
+	{
+		gsKey:  "double-click-distance",
+		xsKey:  "Net/DoubleClickDistance",
+		gsType: gsKeyTypeInt,
+	},
+	{
+		gsKey:  "dnd-drag-threshold",
+		xsKey:  "Net/DndDragThreshold",
+		gsType: gsKeyTypeInt,
+	},
+	{
+		gsKey:  "gtk-cursor-theme-size",
+		xsKey:  "Gtk/CursorThemeSize",
+		gsType: gsKeyTypeInt,
+	},
+	{
+		gsKey:  "gtk-timeout-initial",
+		xsKey:  "Gtk/TimeoutInitial",
+		gsType: gsKeyTypeInt,
+	}, //deprecated
+	{
+		gsKey:  "gtk-timeout-repeat",
+		xsKey:  "Gtk/TimeoutRepeat",
+		gsType: gsKeyTypeInt,
+	}, //deprecated
+	{
+		gsKey:  "gtk-recent-files-max-age",
+		xsKey:  "Gtk/RecentFilesMaxAge",
+		gsType: gsKeyTypeInt,
+	},
+	{
+		gsKey:  "xft-dpi",
+		xsKey:  "Xft/DPI",
+		gsType: gsKeyTypeInt,
+	},
+	{
+		gsKey:  "cursor-blink",
+		xsKey:  "Net/CursorBlink",
+		gsType: gsKeyTypeBool,
+	},
+	{
+		gsKey:  "enable-event-sounds",
+		xsKey:  "Net/EnableEventSounds",
+		gsType: gsKeyTypeBool,
+	},
+	{
+		gsKey:  "enable-input-feedback-sounds",
+		xsKey:  "Net/EnableInputFeedbackSounds",
+		gsType: gsKeyTypeBool,
+	},
+	{
+		gsKey:  "gtk-can-change-accels",
+		xsKey:  "Gtk/CanChangeAccels",
+		gsType: gsKeyTypeBool,
+	}, //deprecated
+	{
+		gsKey:  "gtk-menu-images",
+		xsKey:  "Gtk/MenuImages",
+		gsType: gsKeyTypeBool,
+	},
+	{
+		gsKey:  "gtk-button-images",
+		xsKey:  "Gtk/ButtonImages",
+		gsType: gsKeyTypeBool,
+	},
+	{
+		gsKey:  "gtk-enable-animations",
+		xsKey:  "Gtk/EnableAnimations",
+		gsType: gsKeyTypeBool,
+	},
+	{
+		gsKey:  "gtk-show-input-method-menu",
+		xsKey:  "Gtk/ShowInputMethodMenu",
+		gsType: gsKeyTypeBool,
+	}, //deprecated
+	{
+		gsKey:  "gtk-show-unicode-menu",
+		xsKey:  "Gtk/ShowUnicodeMenu",
+		gsType: gsKeyTypeBool,
+	}, //deprecated
+	{
+		gsKey:  "gtk-auto-mnemonics",
+		xsKey:  "Gtk/AutoMnemonics",
+		gsType: gsKeyTypeBool,
+	}, //deprecated
+	{
+		gsKey:  "gtk-recent-files-enabled",
+		xsKey:  "Gtk/RecentFilesEnabled",
+		gsType: gsKeyTypeBool,
+	},
+	{
+		gsKey:  "gtk-shell-shows-app-menu",
+		xsKey:  "Gtk/ShellShowsAppMenu",
+		gsType: gsKeyTypeBool,
+	},
+	{
+		gsKey:  "xft-antialias",
+		xsKey:  "Xft/Antialias",
+		gsType: gsKeyTypeBool,
+	},
+	{
+		gsKey:  "xft-hinting",
+		xsKey:  "Xft/Hinting",
+		gsType: gsKeyTypeBool,
+	},
+}
+
+func (infos typeGSKeyInfos) getInfoByGSKey(key string) *typeGSKeyInfo {
+	for _, info := range infos {
+		if key == info.gsKey {
+			return &info
+		}
+	}
+
+	return nil
+}
+
+func (infos typeGSKeyInfos) getInfoByXSKey(key string) *typeGSKeyInfo {
+	for _, info := range infos {
+		if key == info.xsKey {
+			return &info
+		}
+	}
+
+	return nil
+}
+
+func (info *typeGSKeyInfo) getKeySType() int8 {
+	switch info.gsType {
+	case gsKeyTypeBool, gsKeyTypeInt:
+		return settingTypeInteger
+	case gsKeyTypeString:
+		return settingTypeString
+	}
+
+	return settingTypeInteger
+}
+
+func (info *typeGSKeyInfo) getKeyValue(s *gio.Settings) interface{} {
+	switch info.gsType {
+	case gsKeyTypeBool:
+		v := s.GetBoolean(info.gsKey)
+		if v {
+			return int32(1)
+		} else {
+			return int32(0)
+		}
+	case gsKeyTypeInt:
+		return int32(s.GetInt(info.gsKey))
+	case gsKeyTypeString:
+		return s.GetString(info.gsKey)
+	}
+
+	return nil
+}
+
+func (info *typeGSKeyInfo) setKeyValue(s *gio.Settings, v interface{}) {
+	switch info.gsType {
+	case gsKeyTypeBool:
+		tmp := v.(int32)
+		if tmp == 1 {
+			s.SetBoolean(info.gsKey, true)
+		} else {
+			s.SetBoolean(info.gsKey, false)
+		}
+	case gsKeyTypeInt:
+		s.SetInt(info.gsKey, v.(int32))
+	case gsKeyTypeString:
+		s.SetString(info.gsKey, v.(string))
+	}
 }
