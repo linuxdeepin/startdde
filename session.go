a5c78ceddc59a152289631975ac522d3644ac112
hubenchang <hubenchang@deepin.com>
2020-06-05 19:55:02 +0800

    fix(session): if waiting for kwin startup can be configured
    https://pms.uniontech.com/zentao/bug-view-20957.html
    
    use 'wm' instead 'kwin_no_scale' as cmd

--- session.go
+++ session.go
@@ -534,10 +534,12 @@ func (manager *SessionManager) listenDBusSignals() {
 	}
 }
 
-func (manager *SessionManager) launchWindowManager(useKwin bool) {
+func (manager *SessionManager) launchWindowManager(wait bool) {
 	if globalUseWayland {
 		return
 	}
+	manager.setPropStage(SessionStageInitBegin)
+	var useKwin bool = shouldUseDDEKwin()
 
 	logger.Debug("Will launch wm")
 	if useKwin {
@@ -545,7 +547,7 @@ func (manager *SessionManager) launchWindowManager(useKwin bool) {
 		if err != nil {
 			logger.Warning(err)
 		}
-		manager.launch("kwin_no_scale", true)
+		manager.launch("kwin_no_scale", wait)
 		return
 	}
 
@@ -555,6 +557,7 @@ func (manager *SessionManager) launchWindowManager(useKwin bool) {
 		return
 	}
 	manager.launch("env", wm.ShouldWait(), "GDK_SCALE=1", wm.GetWM())
+	manager.setPropStage(SessionStageInitEnd)
 }
 
 func (m *SessionManager) launchDDE() {
@@ -600,6 +603,12 @@ func (m *SessionManager) launchDDE() {
 		logger.Debugf("[%d] group p%d start", idx, group.Priority)
 		noWaitCount := 0
 		for _, cmd := range group.Group {
+			if cmd.Command == "wm" {
+				logger.Debug(cmd.Command, cmd.Args, cmd.Wait)
+				m.launchWindowManager(cmd.Wait)
+				continue
+			}
+
 			if cmd.Wait {
 				logger.Debug(cmd.Command, cmd.Args, cmd.Wait)
 				m.launch(cmd.Command, true, cmd.Args...)
@@ -888,12 +897,6 @@ func startSession(conn *x.Conn, useKwin bool,
 	err = initQtThemeConfig()
 	if err != nil {
 		logger.Warning("failed to init qt-theme.ini", err)
-	}
-
-	if !globalUseWayland {
-		manager.setPropStage(SessionStageInitBegin)
-		manager.launchWindowManager(useKwin)
-		manager.setPropStage(SessionStageInitEnd)
 	}
 
 	manager.setPropStage(SessionStageCoreBegin)
