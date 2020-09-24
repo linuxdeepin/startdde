e7fc8369f04db9617598445dd7d7b17dbec657e6
Zhihsian Que <i@zhihsian.me>
2020-09-24 13:52:32 +0800

    fix(diaplay): can't set primary output
    修复无法设置主显示器
    
    Log: 无法设置主显示器
    Bug: https://pms.uniontech.com/zentao/bug-view-49387.html
    Change-Id: I771909a6650094725a03fad7cb08fdd0f7fa0936
    Reviewed-on: http://gerrit.uniontech.com/c/startdde/+/6137
    Reviewed-by: songwentai <songwentai@uniontech.com>
    Reviewed-by: <mailman@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- display/handle_event.go
+++ display/handle_event.go
@@ -1,9 +1,6 @@
 package display
 
 import (
-	"math"
-	"strings"
-
 	x "github.com/linuxdeepin/go-x11-client"
 	"github.com/linuxdeepin/go-x11-client/ext/randr"
 )
@@ -82,87 +79,14 @@ func (m *Manager) handleOutputChanged(ev *randr.OutputChangeNotifyEvent) {
 	m.updateMonitor(ev.Output, outputInfo)
 	m.updatePropMonitors()
 
-	oldMonitorsId := m.monitorsId
-	newMonitorsId := m.getMonitorsId()
-	if newMonitorsId != oldMonitorsId {
-		logger.Debug("new monitors id:", newMonitorsId)
+	oldMonitorsID := m.monitorsId
+	newMonitorsID := m.getMonitorsId()
+	if newMonitorsID != oldMonitorsID {
+		logger.Debug("new monitors id:", newMonitorsID)
 		m.markClean()
 		m.applyDisplayMode()
-		m.monitorsId = newMonitorsId
-		if m.DisplayMode == DisplayModeExtend {
-			m.setHighPriorityMonitorAsPrimaryForExtend(newMonitorsId)
-		}
+		m.monitorsId = newMonitorsID
 	}
-}
-
-func (m *Manager) setHighPriorityMonitorAsPrimaryForExtend(newMonitorsId string) {
-	monitors := strings.Split(newMonitorsId, ",")
-	if len(monitors) >= 2 {
-		logger.Info("new monitors is ", monitors)
-		monitorName, monitorType := m.getHighPriorityMonitorName()
-		monitor, err := m.getOutputPrimary()
-		if err != nil {
-			return
-		}
-		logger.Debug("get high priority monitor is ", monitorName, ",monitor type is ", monitorType, ",cur output monitor is ", m.monitorMap[monitor].Name)
-		if monitorName != "" && !strings.Contains(m.monitorMap[monitor].Name, monitorType) {
-			err := m.setPrimary(monitorName)
-			if err != nil {
-				logger.Warning(err)
-			}
-		}
-	}
-}
-
-func (m *Manager) getHighPriorityMonitorName() (monitorName, monitorType string) {
-	if m.builtinMonitor != nil &&
-		m.builtinMonitor.Connected &&
-		m.DisplayMode == DisplayModeExtend { // 当存在内建屏时，拓展模式下，使用内建屏作为主屏
-		monitorName = m.builtinMonitor.Name
-		mt := strings.Split(monitorName, "-")
-		if len(mt) >= 2 {
-			monitorType = mt[0]
-		}
-		return
-	}
-
-	type Priority struct {
-		name     string
-		priority uint32
-	}
-	// the small 'priority' the value,the higher the priority
-	priorities := []Priority{
-		{name: "eDP", priority: 1},
-		{name: "DP", priority: 2},
-		{name: "HDMI", priority: 3},
-		{name: "DVI", priority: 4},
-		{name: "VGA", priority: 5},
-	}
-	monitorPriority := make([]Priority, 0)
-	monitors := m.getConnectedMonitors()
-	for _, monitor := range monitors {
-		for _, standardMonitor := range priorities {
-			if strings.Contains(monitor.Name, standardMonitor.name) {
-				monitorPriority = append(monitorPriority, Priority{name: monitor.Name, priority: standardMonitor.priority})
-				break
-			}
-		}
-	}
-
-	var highPriority uint32 = math.MaxUint32
-	if len(monitorPriority) >= 1 {
-		for i := 0; i < len(monitorPriority); i++ {
-			if monitorPriority[i].priority < highPriority {
-				highPriority = monitorPriority[i].priority
-				monitorName = monitorPriority[i].name
-				mt := strings.Split(monitorName, "-")
-				if mt != nil {
-					monitorType = mt[0]
-				}
-			}
-		}
-	}
-	return
 }
 
 func (m *Manager) handleOutputPropertyChanged(ev *randr.OutputPropertyNotifyEvent) {
@@ -232,8 +156,6 @@ func (m *Manager) handleScreenChanged(ev *randr.ScreenChangeNotifyEvent) {
 		m.modes = resources.Modes
 	}
 
-	monitorName, _ := m.getHighPriorityMonitorName()
-	m.setPrimary(monitorName)
 	m.updateOutputPrimary()
 
 	logger.Info("redo map touch screen")
