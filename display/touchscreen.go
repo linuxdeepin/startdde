db6cd963e1c8c64f3bccaca3d4d3dff2ea3fdd4c
zsien <i@zsien.cn>
2021-05-06 17:48:00 +0800

    fix(display): 修复显示器旋转后，触摸屏没有跟随旋转
    * 在 CRTC 改变后设置触摸屏关联，无需在设置旋转时设置
    * 改用直接通过 X 修改「Coordinate Transformation Matrix」属性
    
    Log: 修复触摸屏没有跟随显示器旋转
    Influence: 触摸屏关联
    Bug: https://pms.uniontech.com/zentao/bug-view-78109.html
    Change-Id: I33748782f1d3523ec5873a51219457400683254c

--- display/touchscreen.go
+++ display/touchscreen.go
@@ -4,6 +4,7 @@ import (
 	"sync"
 
 	"github.com/godbus/dbus"
+	"github.com/linuxdeepin/go-x11-client/ext/randr"
 	"pkg.deepin.io/dde/api/dxinput"
 	"pkg.deepin.io/dde/api/dxinput/common"
 	dxutils "pkg.deepin.io/dde/api/dxinput/utils"
@@ -12,6 +13,10 @@ import (
 const (
 	BusTypeUnknown uint8 = iota
 	BusTypeUSB
+)
+
+const (
+	rotationReflectAll = randr.RotationReflectX | randr.RotationReflectY
 )
 
 type Touchscreen struct {
@@ -68,30 +73,100 @@ func getXTouchscreenInfo(t *Touchscreen) {
 	}
 }
 
-func (m *Manager) touchScreenSetRotation(direction uint16, output string) {
-	touchSerial := ""
-	for ts, op := range m.TouchMap {
-		if op == output {
-			touchSerial = ts
-			break
-		}
+type TransformationMatrix [9]float32
+
+func (m *TransformationMatrix) set(row int, col int, v float32) {
+	m[row*3+col] = v
+}
+
+func (m *TransformationMatrix) setUnity() {
+	m.set(0, 0, 1)
+	m.set(1, 1, 1)
+	m.set(2, 2, 1)
+}
+
+func (m *TransformationMatrix) s4(x02 float32, x12 float32, d1 float32, d2 float32, mainDiag bool) {
+	m.set(0, 2, x02)
+	m.set(1, 2, x12)
+
+	if mainDiag {
+		m.set(0, 0, d1)
+		m.set(1, 1, d2)
+	} else {
+		m.set(0, 0, 0)
+		m.set(1, 1, 0)
+		m.set(0, 1, d1)
+		m.set(1, 0, d2)
 	}
-	if touchSerial == "" {
-		logger.Errorf("get touchSerial failed")
-		return
+}
+
+func (m *Manager) genTransformationMatrix(offsetX int16, offsetY int16,
+	screenWidth uint16, screenHeight uint16,
+	rotation uint16) TransformationMatrix {
+
+	// total display size
+	width := m.ScreenWidth
+	height := m.ScreenHeight
+
+	x := float32(offsetX) / float32(width)
+	y := float32(offsetY) / float32(height)
+
+	w := float32(screenWidth) / float32(width)
+	h := float32(screenHeight) / float32(height)
+
+	var matrix TransformationMatrix
+	matrix.setUnity()
+
+	/*
+	 * There are 16 cases:
+	 * Rotation X Reflection
+	 * Rotation: 0 | 90 | 180 | 270
+	 * Reflection: None | X | Y | XY
+	 *
+	 * They are spelled out instead of doing matrix multiplication to avoid
+	 * any floating point errors.
+	 */
+	switch int(rotation) {
+	case randr.RotationRotate0:
+		fallthrough
+	case randr.RotationRotate180 | rotationReflectAll:
+		matrix.s4(x, y, w, h, true)
+
+	case randr.RotationReflectX | randr.RotationRotate0:
+		fallthrough
+	case randr.RotationReflectY | randr.RotationRotate180:
+		matrix.s4(x+w, y, -w, h, true)
+
+	case randr.RotationReflectY | randr.RotationRotate0:
+		fallthrough
+	case randr.RotationReflectX | randr.RotationRotate180:
+		matrix.s4(x, y+h, w, -h, true)
+
+	case randr.RotationRotate90:
+		fallthrough
+	case randr.RotationRotate270 | rotationReflectAll: /* left limited - correct in working zone. */
+		matrix.s4(x+w, y, -w, h, false)
+
+	case randr.RotationRotate270:
+		fallthrough
+	case randr.RotationRotate90 | rotationReflectAll: /* left limited - correct in working zone. */
+		matrix.s4(x, y+h, w, -h, false)
+
+	case randr.RotationRotate90 | randr.RotationReflectX: /* left limited - correct in working zone. */
+		fallthrough
+	case randr.RotationRotate270 | randr.RotationReflectY: /* left limited - correct in working zone. */
+		matrix.s4(x, y, w, h, false)
+
+	case randr.RotationRotate90 | randr.RotationReflectY: /* right limited - correct in working zone. */
+		fallthrough
+	case randr.RotationRotate270 | randr.RotationReflectX: /* right limited - correct in working zone. */
+		matrix.s4(x+w, y+h, -w, -h, false)
+
+	case randr.RotationRotate180:
+		fallthrough
+	case rotationReflectAll | randr.RotationRotate0:
+		matrix.s4(x+w, y+h, -w, -h, true)
 	}
 
-	for _, ts := range m.Touchscreens {
-		if ts.Serial != touchSerial {
-			continue
-		}
-		touchScreen, err := dxinput.NewTouchscreen(ts.Id)
-		if err != nil {
-			logger.Warningf("NewTouchScreen %d failed", ts.Id)
-		}
-		err = touchScreen.SetRotation(uint8(direction))
-		if err != nil {
-			logger.Warningf("touchScreen %d SetRotation failed", ts.Id)
-		}
-	}
+	return matrix
 }
