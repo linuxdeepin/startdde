fcb729e942121c8e90c77178650b1ced7f05e038
dengbo <dengbo@uniontech.com>
2021-09-03 10:11:57 +0800

    fix: 加域重启后任务栏不显示
    将init操作移动到构造函数中执行, 防止SessionManager对象创建了, 而相关属性(CurrentSessionPath)还没有初始化
    在其它进程去读取非法的CurrentSessionPath(空)时，Dbus连接会被关闭，导致startdde启动失败
    
    Log: 修复加域重启后任务栏不显示的问题
    Bug: https://pms.uniontech.com/zentao/bug-view-91909.html
         https://pms.uniontech.com/zentao/bug-view-91944.html
    Change-Id: I842b6cd0d83959c1c91234260b234f817876f58f

--- main.go
+++ main.go
@@ -145,7 +145,6 @@ func launchCoreComponents(sm *SessionManager) {
 				handleKWinReady(sm)
 			})
 		} else {
-			initGSettingsConfig()
 			wmCmd := _gSettingsConfig.wmCmd
 			if wmCmd == "" {
 				wmCmd = "x-window-manager"
@@ -212,6 +211,8 @@ func logInfoAfter(msg string) {
 }
 
 func main() {
+	initGSettingsConfig()
+
 	_mainBeginTime = time.Now()
 	flag.Parse()
 
@@ -294,7 +295,6 @@ func main() {
 		}()
 	}
 
-	initGSettingsConfig()
 	go func() {
 		initSoundThemePlayer()
 		playLoginSound()
