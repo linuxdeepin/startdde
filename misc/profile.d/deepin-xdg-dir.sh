e73ca1620787d3bf6d508bcaee37b9a0ba4b9241
justforlxz <justforlxz@gmail.com>
2022-05-27 10:08:38 +0800

    fix: missing XDG_DATA_DIRS environment variables
    set XDG_DATA_DIRS to $HOME/.local/share /usr/local/share /usr/share
    
    Issue: Fixed https://github.com/linuxdeepin/developer-center/issues/2457
    Log:

--- misc/profile.d/deepin-xdg-dir.sh
+++ misc/profile.d/deepin-xdg-dir.sh
@@ -1,7 +1,3 @@
-export XDG_DATA_HOME="$HOME/.local/share"
-export XDG_CONFIG_HOME="$HOME/.config"
-export XDG_CACHE_HOME="$HOME/.cache"
-
 #export XDG_DESKTOP_DIR="$HOME/Desktop"
 #export XDG_DOCUMENTS_DIR="$HOME/Documents"
 #export XDG_DOWNLOAD_DIR="$HOME/Downloads"
