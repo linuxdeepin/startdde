a8fb7c25c44bd1345c60da4b790adbe69366999a
dengbo <dengbo@uniontech.com>
2022-09-14 15:18:45 +0800

    chore: adapt license and copyright
    Modify project to pass the REUSE check
    
    Log: Modify project to pass the REUSE check
    Influence: none
    Task: https://pms.uniontech.com/task-view-185215.html
    Change-Id: Ie4b04a65eb2d20ffefac8085b2e014981f91dae1

--- display/utils/utils.go
+++ display/utils/utils.go
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 package utils
 
 //func GetOutputEDID(conn *x.Conn, output randr.Output) ([]byte, error) {
